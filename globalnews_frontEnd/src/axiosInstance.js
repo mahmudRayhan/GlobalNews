e44fc18735ebc2552503025a30366087a2675dd6
mahmudRayhan <67513247+mahmudRayhan@users.noreply.github.com>
2022-05-25 16:54:12 +0600

    Delete globalnews_frontEnd directory

--- globalnews_frontEnd/src/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create(
-    {
-        baseURL: 'https://global-news-test-default-rtdb.europe-west1.firebasedatabase.app/'
-    }
-);
-export default instance;
--- globalnews_frontEnd/src/components/UI/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from '../../css/Backdrop.module.css';
-
-const backdrop = (props) => (
-    props.show ? <div className={classes.Backdrop} ></div> : null
-);
-
-export default backdrop;
